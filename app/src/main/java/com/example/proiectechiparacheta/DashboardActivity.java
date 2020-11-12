0c67c5a6349aa9a76137c4a5e08a27f3e435d26a
David <santadavid18@stud.ase.ro>
2020-11-12 04:38:12 +0200

    Camera Functionality Added

--- app/src/main/java/com/example/proiectechiparacheta/DashboardActivity.java
+++ app/src/main/java/com/example/proiectechiparacheta/DashboardActivity.java
@@ -1,12 +1,20 @@
 package com.example.proiectechiparacheta;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +26,7 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        ImageButton btnAdd = findViewById(R.id.imageButton);
         ListView listView = (ListView) findViewById(R.id.customListView);
 
         ArrayList<FidelityCard> arrayList = new ArrayList<FidelityCard>();
@@ -36,6 +45,13 @@ public class DashboardActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
         registerForContextMenu(listView);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanBarcode(v);
+            }
+        });
+
 
     }
     @Override
@@ -51,5 +67,29 @@ public class DashboardActivity extends AppCompatActivity {
             menu.add("Three");
             menu.add(card.name);
         }
+    }
+
+    public void scanBarcode(View v){
+        Intent intent = new Intent(this,AddByCamera.class);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==0){
+            if(resultCode== CommonStatusCodes.SUCCESS){
+                if(data!=null){
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    Toast.makeText(this,barcode.displayValue,Toast.LENGTH_LONG);
+                }
+                else{
+                    Toast.makeText(this,"No barcode found!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        else{
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
