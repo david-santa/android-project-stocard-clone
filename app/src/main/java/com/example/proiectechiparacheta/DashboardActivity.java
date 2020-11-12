e012c96c4ccbd1ad14ecedb02ac0868c6788b53b
David <santadavid18@stud.ase.ro>
2020-11-12 02:38:35 +0200

    Added + Button and Edit CMS

--- app/src/main/java/com/example/proiectechiparacheta/DashboardActivity.java
+++ app/src/main/java/com/example/proiectechiparacheta/DashboardActivity.java
@@ -3,6 +3,9 @@ package com.example.proiectechiparacheta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,8 +24,32 @@ public class DashboardActivity extends AppCompatActivity {
         arrayList.add(new FidelityCard(1,"IKEA","David Santa","123123123"));
         arrayList.add(new FidelityCard(2,"MORTIMEI","David Santa","32132131231"));
         arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
+        arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
+        arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
+        arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
+        arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
+        arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
+        arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
+        arrayList.add(new FidelityCard(3,"TEIMORTI","David Santa","69696969"));
 
         CustomAdapter adapter = new CustomAdapter(this, arrayList);
         listView.setAdapter(adapter);
+        registerForContextMenu(listView);
+
+
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        v.setLongClickable(true);
+        if (v.getId() == R.id.customListView) {
+            ListView lv = (ListView) v;
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            FidelityCard card = (FidelityCard) lv.getItemAtPosition(acmi.position);
+
+            menu.add("One");
+            menu.add("Two");
+            menu.add("Three");
+            menu.add(card.name);
+        }
     }
 }
